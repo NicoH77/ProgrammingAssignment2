a95b7e077eb2a67283aebc4b0bca42cfb27c7675
Nicolas Henri <nhenri.pro@gmail.com>
2014-09-18 13:00:03 +0200

    Minor modifications in comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,22 +1,23 @@
-## R Programming
+## Coursera - R Programming
 ## Programming Assignment 2 (peer assessment): Lexical Scoping
 ## 
-## Version : 1.0 -- for submission
+## Version : 1.1 -- Minor changes into the comments
 ##
 ## This script contains two functions that are used to create a special object 
-## that stores a matrix and cache's its inverse
+## that stores a matrix and caches its inverse
+##
 
 
 ## makeCacheMatrix function creates a special matrix
 
 makeCacheMatrix <- function(x = matrix()) {
+    ## Variable initialization  
     m_cache <- NULL
     
     ## Set the value of the matrix
     setMatrix <- function(y) {
         x <<- y
         m_cache <<- NULL
-        ## return(m_cache)
     }
   
     ## Get the value of the matrix
@@ -28,6 +29,7 @@ makeCacheMatrix <- function(x = matrix()) {
     ## Get the value of the inverse matrix
     getInverseMatrix <- function() m_cache
     
+    ## Create the list
     list(setMatrix = setMatrix, getMatrix = getMatrix,
        setInverseMatrix = setInverseMatrix,
        getInverseMatrix = getInverseMatrix)
@@ -38,7 +40,7 @@ makeCacheMatrix <- function(x = matrix()) {
 ## cacheSolve function calculate the inverse of matrix
 
 cacheSolve <- function(x, ...) {
-
+    ## Variable initialization 
     m_cache <- x$getInverseMatrix()
     
     ## if the inverse of X is already in cache, then it simply returns the cache
@@ -47,7 +49,7 @@ cacheSolve <- function(x, ...) {
         return(m_cache)
     }
   
-    ## if the inverse is not in cache it calculates it and puts it into the cache
+    ## if the inverse is not in cache, then it calculates it, and puts it into the cache
     MatrixData <- x$getMatrix()
     m_cache <- solve(MatrixData)
     x$setInverseMatrix(m_cache)
